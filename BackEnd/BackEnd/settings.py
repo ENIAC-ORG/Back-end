fa9560771902a9a418d528d88078645656c59d83
ZeynabBaghiyan <hengamehb1382@gmail.com>
2024-12-13 04:30:23 +0330

    fix/Add more question+ change match

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = [
     "chat",
     "depression_chat",
     "recomendationSys",
+    'RecomendationSystem',
 ]
 
 # INSTALLED_APPS = [
@@ -208,29 +209,29 @@ ASGI_APPLICATION = 'BackEnd.asgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'TherapyDB',
-        'USER': 'postgres',
-        'PASSWORD': 'Hgbr@@5391',
-        'HOST': 'postgreSQL',
-        # 'HOST': 'localhost',
-        'PORT': '5432',
-        'DISABLE_SERVER_SIDE_CURSORS': True,
-        'TEST': {
-            'NAME': 'test_TherapyDB', 
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'TherapyDB',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Hgbr@@5391',
+#         'HOST': 'postgreSQL',
+#         # 'HOST': 'localhost',
+#         'PORT': '5432',
+#         'DISABLE_SERVER_SIDE_CURSORS': True,
+#         'TEST': {
+#             'NAME': 'test_TherapyDB', 
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
