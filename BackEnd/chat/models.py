cf6a2f9e9c522a3fc32e5cab675ed13abec4c834
root <root@srv5597303878.bitcommand.net>
2024-12-25 15:21:01 +0000

    fix/database-bugs

--- BackEnd/chat/models.py
+++ BackEnd/chat/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from accounts.models import User
-
+from datetime import datetime
 # مدل گروه چت
 class Room(models.Model):
     title = models.CharField(max_length=100)
@@ -16,7 +16,7 @@ class Message(models.Model):
     room = models.ForeignKey(Room, related_name="messages", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return f"{self.user.username}: {self.content[:20]}"
--- BackEnd/docker-entrypoint.sh
+++ BackEnd/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 
 echo "Waiting for postgresql to start..."
 ./wait-for.sh db:5432
+
 apps=(
     "accounts"
     "Profile"
@@ -12,25 +13,29 @@ apps=(
     "TherapyTests"
     "Doctorpanel"
     "Rating"
-    "channels"
     "chat"
     "depression_chat"
+    "GoogleMeet"
+    "RecomendationSystem"
     "recomendationSys"
 )
+
 
 echo "--------------------------- Migrating the databse ---------------------------"
 for app in "${apps[@]}"; do
     echo "Running makemigrations for app: $app"
     python manage.py makemigrations "$app" --noinput
 done
-python manage.py migrate
-# python manage.py migrate --noinput
+
+echo "-------------------------- migrate -----------------------------------------" 
+#python manage.py migrate
+python manage.py migrate --noinput
 
 echo "--------------------------- Set Admin ---------------------------"
 DJANGO_SUPERUSER_PASSWORD=eniac@1403 python manage.py createsuperuser --no-input  --email=eniakgroupiust@gmail.com
 
 echo "--------------------------- Starting the Server ---------------------------"
 # python -u manage.py runserver 0.0.0.0:8000
-gunicorn BackEnd.asgi:application -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+gunicorn BackEnd.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
 
 
