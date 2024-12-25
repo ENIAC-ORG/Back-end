7019d80e5903bc59d98296c38fc764d66071f3f4
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-12-25 14:50:17 +0330

    fix/user-auth

--- BackEnd/BackEnd/asgi.py
+++ BackEnd/BackEnd/asgi.py
@@ -7,19 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat import routing
+from chat.routing import websocket_urlpatterns  # Import the WebSocket URL patterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BackEnd.settings')
+
+# Initialize the Django ASGI application
+django_asgi_app = get_asgi_application()
+
+# Define the ASGI application
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,  # Handles HTTP requests
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            routing.websocket_urlpatterns
+            websocket_urlpatterns  # Use the WebSocket URL patterns from chat.routing
         )
     ),
 })
