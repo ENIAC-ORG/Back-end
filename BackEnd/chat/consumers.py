9c042ab78d1130faca4c56b22ecb4486db5640e0
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-12-26 01:45:14 +0330

    fix/chat-consumer

--- BackEnd/chat/consumers.py
+++ BackEnd/chat/consumers.py
@@ -3,8 +3,8 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from accounts.models import User
+# User = get_user_model()
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
