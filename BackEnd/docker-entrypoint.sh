1028a8de607b1527c36a95beb2719b1f3b75eddc
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-12-26 09:26:32 +0330

    fix/chat-consumer

--- BackEnd/docker-entrypoint.sh
+++ BackEnd/docker-entrypoint.sh
@@ -36,6 +36,6 @@ DJANGO_SUPERUSER_PASSWORD=eniac@1403 python manage.py createsuperuser --no-input
 
 echo "--------------------------- Starting the Server ---------------------------"
 # python -u manage.py runserver 0.0.0.0:8000
-# gunicorn BackEnd.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
-gunicorn BackEnd.asgi:application --bind 0.0.0.0:8000 --log-level debug
+gunicorn BackEnd.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --log-level debug
+# gunicorn BackEnd.asgi:application --bind 0.0.0.0:8000 --log-level debug
 
