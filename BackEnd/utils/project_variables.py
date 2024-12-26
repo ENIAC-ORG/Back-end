835bf8fc56d5c89a6bd4241c689a0a9d6853bd03
Asma Hamid <asmahamid1382@gmail.com>
2024-12-26 15:11:09 +0330

    fix/googlemeet/8

--- BackEnd/utils/project_variables.py
+++ BackEnd/utils/project_variables.py
@@ -1,10 +1,10 @@
 MAX_VERIFICATION_TRIES = 5
 GOOGLE_CLIENT_SECRETS_FILE = 'credentials.json'
 SCOPES = [
-    "https://www.googleapis.com/auth/calendar",
-    "https://www.googleapis.com/auth/calendar.events",
     "openid",
-    "email"
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/calendar.events"
 ]
 
 
