8f284f9360c6684457eb5b92a7b395dd0f003744
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-12-27 18:42:44 +0330

    fix/installed-apps

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -111,6 +111,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework_simplejwt',
     "rest_framework",
     "rest_framework_swagger",
     "rest_framework_simplejwt.token_blacklist",
