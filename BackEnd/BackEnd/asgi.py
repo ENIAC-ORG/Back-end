0bcbead931cfe1a218156f72e6a2fb6819638e83
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-12-27 11:29:09 +0330

    fix/adjust-asgi-urls

--- BackEnd/BackEnd/asgi.py
+++ BackEnd/BackEnd/asgi.py
@@ -12,7 +12,8 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns  # Import the WebSocket URL patterns
-
+from chat.consumers import ChatConsumer 
+from django.urls import path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BackEnd.settings')
 
 # Initialize the Django ASGI application
@@ -23,7 +24,8 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,  # Handles HTTP requests
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns  # Use the WebSocket URL patterns from chat.routing
+            path("ws/chat/<str:room_name>/", ChatConsumer.as_asgi()),
         )
     ),
 })
+
