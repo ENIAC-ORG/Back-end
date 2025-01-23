1babc19c4ad1dd6f345fe1f776e258d77fd9e584
root <root@srv5597303878.bitcommand.net>
2025-01-23 13:10:56 +0000

    fix/crontab

--- BackEnd/reservation/cron.py
+++ BackEnd/reservation/cron.py
@@ -5,27 +5,33 @@ from django.urls import reverse
 from .models import Reservation
 from django.conf import settings
 # from celery import shared_task
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def check_and_send_feedback_email():
     """
     بررسی رزروهای گذشته و ارسال ایمیل یادآوری به پزشک.
     """
-    reservations = Reservation.objects.filter(date__lt=now().date(), feedback__isnull=True, email_sent=False)
+
+    reservations = Reservation.objects.filter(date__lt=now().date(), feedback__isnull=True,email_sent=False)
     for reservation in reservations:
         feedback_url = reverse('api_feedback', args=[reservation.id])
         full_url = f'{settings.WEBSITE_URL}{feedback_url}'
         send_mail(
             subject='یادآوری: ارسال فیدبک جلسه',
             message=(
-                f'''دکتر {reservation.psychiatrist.name} عزیز،
+                f'''دکتر {reservation.psychiatrist.get_fullname()} عزیز،
                 لطفاً فیدبک جلسه مورخ {reservation.date} ساعت {reservation.time} را از طریق لینک زیر ثبت کنید:
                 {full_url}'''
             ),
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[reservation.psychiatrist.email],
+            recipient_list=[reservation.psychiatrist.user.email],
             fail_silently=False,
         )
+        print( "hello " ) 
+        logger.warning( f" here for email : {reservation.psychiatrist.user.email}" )
         reservation.email_sent = True
         reservation.save()
 
